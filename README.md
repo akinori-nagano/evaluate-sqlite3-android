9cc5af3bbb6c31a2323bfea2b95c514126749a5c
nagano <nagano.akinori@gmail.com>
2021-06-03 11:40:16 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # evaluate-sqlite3-android
+
+# ビルド方法
+
+以下のコマンドでC言語ファイルをビルドし共有ライブラリを作成する
+
+```
+$ sh ./ndk-build.sh
+```
+
+Android Studio などから app を作成する
--- ndk-build.sh
+++ ndk-build.sh
@@ -9,12 +9,18 @@ set -ue
 
 export NDK_PROJECT_PATH="${SCRIPT_DIR}"
 
-arch -x86_64 /bin/bash -c \
+if [ `uname` = "Darwin" ] && [ `uname -m` = "arm64" ]; then
+	### M1 mac の場合、rosetta2 で x86バイナリを実行する必要がある
+	arch -x86_64 /bin/bash -c \
+		ndk-build \
+		NDK_PROJECT_PATH=${NDK_PROJECT_PATH} \
+		APP_BUILD_SCRIPT=${NDK_PROJECT_PATH}/Android.mk \
+		NDK_APPLICATION_MK=${NDK_PROJECT_PATH}/Application.mk
+else
 	ndk-build \
-	NDK_PROJECT_PATH=${NDK_PROJECT_PATH} \
-	APP_BUILD_SCRIPT=${NDK_PROJECT_PATH}/Android.mk \
-	NDK_APPLICATION_MK=${NDK_PROJECT_PATH}/Application.mk
-
-#arch -x86_64 /bin/bash -c ndk-build
+		NDK_PROJECT_PATH=${NDK_PROJECT_PATH} \
+		APP_BUILD_SCRIPT=${NDK_PROJECT_PATH}/Android.mk \
+		NDK_APPLICATION_MK=${NDK_PROJECT_PATH}/Application.mk
+fi
 
 cp -pr ./libs/* app/src/main/jniLibs/
